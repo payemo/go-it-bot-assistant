72d53c5efcbecc824ef934c88b01e9595634b3f0
Anton Sizov <113884978+Bigtoxa766@users.noreply.github.com>
2024-08-15 23:00:48 +0300

    A.sizov/bot 14 (#8)
    * Notes added
    
    * notes added
    
    * edit_notes
    
    * v.2.0

---
+++ src/notes_manager.py
@@ -0,0 +1,18 @@
+
+
+"""Note manager"""
+class Note:
+    def __init__(self, content):
+        self.content = self.format_note(content)
+
+    def edit(self, new_content):
+        self.content = self.format_note(new_content)
+        return f"Note updated to: {self.content}"
+
+    @staticmethod
+    def format_note(note, line_length=50):
+        """Format the note to have new lines every `line_length` characters."""
+        return '\n'.join([note[i:i + line_length] for i in range(0, len(note), line_length)])
+
+    def __str__(self):
+        return self.content
