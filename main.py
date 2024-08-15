09206105ddbc1b627e274a67921afb50a0f36155
e.moninets <e.moninets@cleack.com>
2024-08-15 18:02:30 +0300

    Merge team development. Add data_manager packet.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src.cmd_parser import CommandParser
 from src.cmd_handlers import HandlerResponse
+from src.data_manager import DataManager
 
 def main():
+    #dm = DataManager()
+    #data = dm.load_data()
+
     while True:
         cmd = input('Enter the command (use \'help\' to list all available commands): ')
         response = CommandParser.parse(cmd).handle_input()
@@ -11,6 +15,8 @@ def main():
 
         if response.status == HandlerResponse.Status.FINISH:
             break
+    
+    #dm.save_data(data)
 
 if __name__ == '__main__':
     main()
