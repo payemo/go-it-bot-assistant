8fd4a8f7bc69471b3f612d4e71cd96ea800da65b
Andrii Vynar <andrii.vynar.om@gmail.com>
2024-08-15 22:22:55 +0300

    Added fields validation

--- src/fields.py
+++ src/fields.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 class Field:
     def __init__(self, value) -> None:
         self._validate(value)
@@ -10,21 +12,62 @@ class Field:
         raise NotImplementedError(f"[Field.{self._validate.__name__}] not implemented.")
     
 class Name(Field):
+    def __init__(self, value) -> None:
+        self._validate(value)
+        self._value = value
+
     def _validate(self, value: str):
         pass
     
 class Phone(Field):
+    required_num_of_digits = 10
+
+    def __init__(self, value) -> None:
+        self._validate(value)
+        self._value = value
+
+    def __str__(self) -> str:
+        return str(self._value)
+    
     def _validate(self, value: str):
-        pass
+        try:
+            if int(value) and len(value) != Phone.required_num_of_digits:
+                raise ValueError("Phone not equals 10 digits")
+        except:
+            raise ValueError("All characters are not digits")
+        
 
 class Address(Field):
+    def __init__(self, value) -> None:
+        self._validate(value)
+        self._value = value
+
     def _validate(self, value: str):
         pass
 
 class Email(Field):
+    def __init__(self, value) -> None:
+        self._validate(value)
+        self._value = value
+
+    def __str__(self) -> str:
+        return str(self._value)
+
     def _validate(self, value: str):
         pass
 
 class Birthday(Field):
+    def __init__(self, value) -> None:
+        self._validate(value)
+        self._value = value
+
+    def __str__(self) -> str:
+        return str(self._value)
+
     def _validate(self, value: str):
-        pass
+        try:
+            date = datetime.strptime(value, "%d.%m.%Y").date()
+            if date > datetime.today().date():
+                raise ValueError("Birthdate is from the future")
+        except:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
